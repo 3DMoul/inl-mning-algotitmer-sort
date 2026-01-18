17a78680811ce233f1aab06fb0ada91fedde18ba
Svante Norrhäll <svanteman123@yahoo.se>
2026-01-18 15:19:45 +0100

    Fixed enum

--- inlämning-algoritmer/eventmanager.cpp
+++ inlämning-algoritmer/eventmanager.cpp
@@ -1,22 +1,59 @@
 #include "eventmanager.h"
 
-Event::Event(eventType type, std::string UnitOfMeasurment, std::string SensorName, double MinSimulation, double MaxSimulation)
+Event::Event(EventType type, std::string UnitOfMeasurment, std::string SensorName, double MinSimulation, double MaxSimulation)
 {
 }
 
-Event::eventType() const
+double Event::Read() const
 {
-	return eventType();
+	return 0.0;
 }
+
+double Event::minValue() const
+{
+	return 0.0;
+}
+
+double Event::maxValue() const
+{
+	return 0.0;
+}
+
+std::string Event::GetUnitOfMeasurement() const
+{
+	return std::string();
+}
+
+EventType Event::type() const
+{
+	return EventType();
+}
+
+
 
 eventType::TemperatureReading::TemperatureReading(std::string NewName, double MinSimulation, double MaxSimulation)
+	: Event(EventType::TemperatureReading, "C", NewName, MinSimulation, MaxSimulation) {
+};
+
+double eventType::TemperatureReading::Read() const
 {
+	return 0.0;
 }
 
 eventType::ButtonPress::ButtonPress(std::string NewName, double MinSimulation, double MaxSimulation)
-{
+	: Event(EventType::ButtonPress, "Time pressed", NewName, MinSimulation, MaxSimulation) {
 }
+double eventType::ButtonPress::Read() const
+{
+	return 0.0;
+}
+;
 
 eventType::MotionRecord::MotionRecord(std::string NewName, double MinSimulation, double MaxSimulation)
-{
+	: Event(EventType::MotionRecord, "Activity class", NewName, MinSimulation, MaxSimulation) {
 }
+double eventType::MotionRecord::Read() const
+{
+	return 0.0;
+}
+;
--- inlämning-algoritmer/eventmanager.h
+++ inlämning-algoritmer/eventmanager.h
@@ -1,47 +1,45 @@
 #pragma once
 #include "iostream"
 #include "string"
-enum class eventType { TemperatureReading = 1, ButtonPress = 2, MotionRecord = 3};
+
+enum class EventType { TemperatureReading = 1, ButtonPress = 2, MotionRecord = 3 };
 
 class Event
 {
 public:
-	Event(eventType type, std::string UnitOfMeasurment, std::string SensorName, double MinSimulation, double MaxSimulation);
-	virtual ~Event() = default;
-	virtual double Read() const;
-	std::string name() const;
-	double minValue() const;
-	double maxValue() const;
-	std::string GetUnitOfMeasurment() const;
-	eventType eventType()const;
+    Event(EventType type, std::string UnitOfMeasurement, std::string SensorName, double MinSimulation, double MaxSimulation);
+    virtual ~Event() = default;
+    virtual double Read() const;
+    std::string name() const;
+    double minValue() const;
+    double maxValue() const;
+    std::string GetUnitOfMeasurement() const;
+    EventType type() const;
 private:
-	int timestamp;
-	int sensorId;
-	int value;
-	eventType type;
+    int timestamp_;
+    int sensorId_;
+    int value_;
+    EventType type_;
 };
 
 namespace eventType
 {
-	class TemperatureReading : public Event
-	{
-	public:
-		TemperatureReading(std::string NewName, double MinSimulation, double MaxSimulation);
-		//har get denna för att airquality behöver inte decimaler i sina measurments
-		double Read() const override;
-	};
-	class ButtonPress : public Event
-	{
-	public:
-		ButtonPress(std::string NewName, double MinSimulation, double MaxSimulation);
-		//denna ger measurment med decimaler
-		double Read() const override;
-	};
-	class MotionRecord : public Event
-	{
-	public:
-		MotionRecord(std::string NewName, double MinSimulation, double MaxSimulation);
-		//har get denna för att Humidity behöver inte decimaler i sina measurments
-		double Read() const override;
-	};
+    class TemperatureReading : public Event
+    {
+    public:
+        TemperatureReading(std::string NewName, double MinSimulation, double MaxSimulation);
+        double Read() const override;
+    };
+    class ButtonPress : public Event
+    {
+    public:
+        ButtonPress(std::string NewName, double MinSimulation, double MaxSimulation);
+        double Read() const override;
+    };
+    class MotionRecord : public Event
+    {
+    public:
+        MotionRecord(std::string NewName, double MinSimulation, double MaxSimulation);
+        double Read() const override;
+    };
 }
